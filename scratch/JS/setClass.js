e3283c30c56f6becd4357b6aec52d19a1ff88cb0
Cheima Bessouda <cheima.bessouda@powercoders.org>
2018-12-20 13:50:31 +0100

    javascripUpload

--- scratch/JS/setClass.js
+++ scratch/JS/setClass.js
@@ -5,17 +5,31 @@ function greet(name) {
   console.log('Hello, ' + name);  // le nom sera rajouté dans "console" sur le navigsrateur
 }
 
-function setClassTip(event) {
+
+function setClass(nomDeClasse) {
   let el = document.getElementById('mypara');
-  el.className = 'tip';
+  el.className = nomDeClasse;
 }
 
-function setClassWarning(event) {
-  let el = document.getElementById('mypara');
-  el.className = 'warning';
-}
+document.addEventListener('DOMContentLoaded', function(event) {
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event) {
+    setClass('warning');
+ });
 
-function clearClass(event) {
-  let el = document.getElementById('mypara');
-  el.className = ''; // Quand rien n arrive ne rien mettre dans les guillemets
-}
+  let makeTip = document.getElementById('make-tip');
+  makeTip.addEventListener('click', function(event) {
+    setClass('tip');
+  });
+
+  let makeNormal  = document.getElementById('make-normal');
+  makeNormal.addEventListener('click', function(event) {
+    setClass('');
+  });
+});
+
+
+
+
+// setclass c est le nom de la fonction qu'on choisit librement
+// className c e  st une variable, le nom est choisi librement

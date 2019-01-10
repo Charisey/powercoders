30446823b3aea2c8415b8cacea38bf16e6cfc88a
Cheima Bessouda <cheima.bessouda@powercoders.org>
2019-01-10 09:03:14 +0100

    Add and delete List Items
    - Create a List Item with Span Element and Delete Button
    - Delete List item when the Delete Button is clicked

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,6 +1,29 @@
-document.addEventListener('DOMContentLoaded' , function(event) {
-    document.querySelector('button').addEventListener('click', function(event){
-        console.log(document.getElementById('item').value);
-    });
+function createNewListItem(itemName) {
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+  span.innerText = itemName;
+  li.appendChild(span);
 
+  let deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Delete';
+  li.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', function (event) {
+    li.remove();
+    console.log('Delete button clicked: ' + itemName);
+  });
+
+  document.querySelector('ul').appendChild(li);
+
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('button').addEventListener('click', function (event) {
+    let textBox = document.getElementById('item');
+    console.log(textBox.value);
+    let li = createNewListItem(textBox.value);
+    let ul = document.querySelector('ul');
+    ul.appendChild(li);
+  });
 });

edc91fc41a77e304dd9327033a21a1524b90cab5
RezaW22 <39699164+RezaW22@users.noreply.github.com>
2019-02-04 11:41:57 +0100

    Use new style classes to replace current constructor with new style

---
+++ shopping-class/shopping.js
@@ -0,0 +1,121 @@
+/**
+ * Represent an item in teh shopping list.
+ */
+class ShoppingListItem{
+    /**
+     *
+     * @param name {string} Name
+     * @param quantity {string} Quantity
+     */
+    constructor(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+    /**
+     * Creates and returns an 'li' element for inclusion in the shopping list.
+     *
+     * @returns {HTMLElement} li element
+     */
+    toListItem () {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.innerText = this.name;
+        li.appendChild(span);
+
+        if (this.quantity !== '') {
+            li.appendChild(document.createTextNode(' '));
+            const qSpan = document.createElement('span');
+            qSpan.innerText = '(' + this.quantity + ')';
+            li.appendChild(qSpan);
+        }
+
+        const deleteButton = document.createElement('i');
+        deleteButton.className = 'fas fa-trash-alt';
+        li.appendChild(deleteButton);
+
+        deleteButton.addEventListener('click', function () {
+            li.remove();
+            document.getElementById("item").focus();
+
+            const listItem = document.querySelectorAll('li');
+            const clearListButton = document.querySelector('#clear');
+            clearListButton.disabled = listItem.length === 0;
+        });
+
+        document.querySelector('ul').appendChild(li);
+        return li;
+    }
+}
+
+function domContentLoaded() {
+    const inputBox = document.getElementById('item');
+    const shoppingList = document.querySelector('ul');
+    const addItemButton = document.querySelector('#buttonAdd'); //s'il y a une id
+    const clearListButton = document.querySelector('#clear');
+    const quantityBox = document.getElementById('quantity');
+
+    addItemButton.disabled = true;
+    clearListButton.disabled = true;
+    inputBox.className = 'empty';
+
+    addItemButton.addEventListener('click', function () {
+        const trimmedValue = inputBox.value.trim();
+
+        const item = new ShoppingListItem(trimmedValue, quantityBox.value.trim());
+
+        shoppingList.appendChild(item.toListItem());
+        inputBox.value = '';
+        addItemButton.disabled = true;
+        clearListButton.disabled = true;
+        quantityBox.value = '';
+        inputBox.className = 'empty';
+        inputBox.focus();
+    });
+
+    inputBox.addEventListener('keyup', onKeyup);
+
+    quantityBox.addEventListener('keyup', onKeyup);
+
+    clearListButton.addEventListener('click', function () {
+        const items = document.querySelectorAll('li');
+        items.forEach(function (element) {
+            element.remove();
+        });
+        inputBox.focus();
+        clearListButton.disabled = true;
+    });
+    inputBox.focus();
+
+    function onKeyup(event) {
+        const trimmedValue = inputBox.value.trim();
+        addItemButton.disabled = trimmedValue === '';
+
+        if (trimmedValue === '') {
+            inputBox.className = 'empty';
+            return;
+        }
+
+        if (event.key !== 'Enter') {
+            inputBox.className = '';
+            return;
+        }
+
+        const item = new ShoppingListItem(trimmedValue, quantityBox.value.trim());
+
+        shoppingList.appendChild(item.toListItem());
+        inputBox.value = '';
+        addItemButton.disabled = true;
+        clearListButton.disabled = false;
+        quantityBox.value = '';
+        inputBox.className = 'empty';
+    }
+
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', function () {
+        domContentLoaded();
+    });
+} else {
+    domContentLoaded();
+}
